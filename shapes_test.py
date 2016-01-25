b49d54ad0ef46389d827d666d4d45031b5a27c0f
Neil Birkbeck <neil.birkbeck@gmail.com>
2016-01-24 19:18:41 -0800

    Some more comments.

--- shapes_test.py
+++ shapes_test.py
@@ -17,7 +17,8 @@ class TestCircle(unittest.TestCase):
         self.assertTrue(circle.IsInside(1, 0))
         self.assertTrue(circle.IsInside(0, 1))
         self.assertFalse(circle.IsInside(0, 1.1))
-        
+
+    # TODO(luke): add more tests (for other shapes and methods)
         
 if __name__ == '__main__':
     unittest.main()
